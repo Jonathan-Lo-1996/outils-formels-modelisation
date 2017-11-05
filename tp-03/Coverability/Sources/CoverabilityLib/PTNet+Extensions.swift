8c31d151f2907b538f1c6e1ee1ccf12103ca2b64
Jonathan-Lo-1996 <Jonathan.Lo@etu.unige.ch>
2017-11-05 15:39:44 +0100

    add tp-03

--- tp-03/Coverability/Sources/CoverabilityLib/PTNet+Extensions.swift
+++ tp-03/Coverability/Sources/CoverabilityLib/PTNet+Extensions.swift
@@ -13,6 +13,17 @@ public extension PTNet {
         // print debug information you'll write in that function will NOT be taken into account to
         // evaluate your homework.
 
+	// Cette fonction retourne un element du type CoverabilityGraph
+	
+	let m = marking // C'est le marquage initial
+	let m0 = CoverabilityGraph(marking: m) // C'est le CoverabilityGraph initial
+	var next = [CoverabilityGraph]() // Tableau contenant les prochains elements a analyser
+	next.insert(m0, at: 0) // Nous mettons le CoverabilityGraph initial dans la liste de ceux a analyser
+	var seen = [CoverabilityGraph]() // Tableau vide pour stocker les elements deja analyse
+	
+	// Nous parcourons les CoverabilityGraph :
+	
+
         return CoverabilityGraph(marking: marking)
     }
 
